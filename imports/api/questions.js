7491822a0ec62480ea6ce56f403fba28a04f99b8
JavierTrc <ja.troconis10@uniandes.edu.co>
2017-10-01 18:52:50 -0500

    Changed the configuration file to use 2 spaces for indentation, and added the schema for questions

--- imports/api/questions.js
+++ imports/api/questions.js
@@ -1,6 +1,46 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
+import SimpleSchema from 'simpl-schema';
+
+
+const countryOptSchema = new SimpleSchema({
+  countryCode: String,
+  countryName: String,
+  count: SimpleSchema.Integer
+});
+
+const optionSchema = new SimpleSchema({
+  name: String,
+  count: SimpleSchema.Integer,
+  countries: [countryOptSchema]
+});
+
+const ratingSchema = new SimpleSchema({
+  rating:Number,
+  count:SimpleSchema.Integer,
+});
+
+const commentSchema = new SimpleSchema({
+  authorId: String,
+  authorName: String,
+  text: String,
+  createdAt: Date,
+  rating: ratingSchema,
+});
+
+const questionSchema = new SimpleSchema({
+  question: String,
+  description:String,
+  publishedAt: Date,
+  options: [optionSchema],
+  ownerId: String,
+  ownerName: String,
+  comments: [commentSchema],
+  categories: [String],
+  rating:ratingSchema,
+
+});
 
 export const Questions = new Mongo.Collection('questions');
 
