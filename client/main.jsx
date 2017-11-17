c78b8922a428aa4e664b6a95d418c2d411dcae97
Juan Camilo Bages <jcbages@users.noreply.github.com>
2017-11-17 11:28:18 -0500

    Agregar jquery para  atrbuto lang en html

--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,9 @@ import App from '../imports/ui/App.jsx';
 import Nav from "../imports/ui/Nav.jsx";
 
 Meteor.startup(() => {
+  // puedes usar jquery para agregar el atributo lang a htnl
+  // esto porque axe y lighthouse molestan por ese atributo
+  $('html').attr({lang: 'en'});
   render(<Nav />, document.getElementById("header-target"));
   Meteor.subscribe("users")
   Meteor.subscribe("Meteor.users.country");
